6e990c8f9354fa025214dbf395d1bdd841780877
douglasmonteles <silvadouglas328@gmail.com>
2023-05-02 13:04:22 -0300

    Add game logic

---
+++ termo/src/GameManager.hs
@@ -0,0 +1,74 @@
+module GameManager
+    ( initGame
+    ) where
+
+import Input(
+    readString
+    )
+
+import System.Exit (exitWith, ExitCode(..))
+
+import Validators (
+    isLengthCorrect,
+    isSameString,
+    isAnyWordInRightPlace,
+    isAnyWordInWrongPlace,
+    obtainIndexesOfSameLetter,
+    obtainLettersInWrongPlace
+    )
+
+initGame :: String -> Int -> IO ()
+initGame answer attempts = 
+    do 
+        putStrLn $ "Tentativa: [" ++ (show attempts) ++ "]"
+        putStrLn "Digite seu palpite (com 6 letras):"
+        word <- readString
+        putStrLn "A palavra digitada foi:"
+        putStrLn word
+        putStrLn "A palavra digitada tem o mesmo tamanho da palavra correta?"
+        
+        if isLengthCorrect word answer 
+            then do
+                putStrLn "Sim!"
+            else do
+                putStrLn "A palavra digitada não possui o mesmo tamanho da palavra a ser adivinhada! Tente novamente."
+                initGame answer (attempts + 0)
+
+        if isSameString word answer
+            then do
+                putStrLn "Acertou mizeravi!"
+            else do
+                if isAnyWordInRightPlace word answer
+                    then do
+                        putStrLn "Há letras em comum nas mesmas posições!"
+                        putStrLn $ "As letras iguais estão nas posições: " ++ show (obtainIndexesOfSameLetter word answer)
+                        displayAnswerSituation answer (obtainIndexesOfSameLetter word answer)
+                    else do
+                        putStrLn "Não há letras em comum na mesma posição"
+                        -- initGame answer (attempts + 1)
+                
+                if isAnyWordInWrongPlace word answer
+                    then do
+                        putStrLn "As letras em comum estão em posições diferentes!"
+                        putStrLn $ "As letras que estão no lugar errado são: " ++ (obtainLettersInWrongPlace word answer)
+                        initGame answer (attempts + 1)
+                    else do
+                        putStrLn "Não há letras em comum"
+                        initGame answer (attempts + 1)
+
+        putStrLn "A resposta correta é:"
+        putStrLn answer
+        exitWith ExitSuccess
+
+displayAnswerSituation :: String -> [Int] -> IO ()
+displayAnswerSituation answer indexes =
+  do 
+    let chars = zip [0..] answer in
+        mapM_ (printIndexedChar indexes) chars;
+    putStrLn ""
+
+printIndexedChar :: [Int] -> (Int, Char) -> IO ()
+printIndexedChar indexes (i, c) =
+  if i `elem` indexes
+    then putChar c
+    else putChar '_'
