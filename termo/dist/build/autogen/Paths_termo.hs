6768dc13cea4ae28474a70e6f75dcc478c1b69c9
gabrielm2q <mariano.gabriel2q@gmail.com>
2023-05-04 11:42:44 -0300

    Improve GameScore Print and Save Logic
    Co-authored-by: felipecdmoura <felipecandidodemoura@gmail.com>

---
+++ termo/dist/build/autogen/Paths_termo.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_termo (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/usr/local/bin"
+libdir     = "/usr/local/lib/x86_64-linux-ghc-8.8.4/termo-0.1.0.0-JFbbWVuXtdt44pRWhyKBdF"
+dynlibdir  = "/usr/local/lib/x86_64-linux-ghc-8.8.4"
+datadir    = "/usr/local/share/x86_64-linux-ghc-8.8.4/termo-0.1.0.0"
+libexecdir = "/usr/local/libexec/x86_64-linux-ghc-8.8.4/termo-0.1.0.0"
+sysconfdir = "/usr/local/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "termo_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "termo_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "termo_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "termo_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "termo_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "termo_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
--- termo/src/GameScore.hs
+++ termo/src/GameScore.hs
@@ -5,7 +5,7 @@ main :: IO()
 main = do
     createGameScore
 
-    saveGameScore True 3
+    saveGameScore 1 3
 
 createGameScore :: IO()
 createGameScore = do
@@ -13,13 +13,43 @@ createGameScore = do
     fileExists <- doesFileExist fileName
 
     if not fileExists
-        then
+        then do
             appendFile fileName "0\n0\n0"
         else
             return() 
             
 
-saveGameScore :: Bool -> Int -> IO()
+saveGameScore :: Int -> Int -> IO()
 saveGameScore victory nAttempts = do
-    content <- readFile "GameScore.txt"
-    putStr content[0]
+    let fileName = "GameScore.txt"
+    let scoreSize = 3
+    content <- readScoreList fileName
+    let points = 6000 - (1000 * nAttempts)
+    let newContent = zipWith(+) content [1,victory,points]
+    printScore newContent scoreSize
+    
+printScore :: [Int] -> Int -> IO()
+printScore _ 0 = printLine
+printScore (x:xs) n
+    | n == 1 = do
+        putStr "Score: "
+        print x
+        printScore xs (n-1) 
+    | n == 2 = do
+        putStr "Vitorias: "
+        print x
+        printScore xs (n-1)
+    | n == 3 = do
+        printLine
+        putStr "Numero de Jogos: "
+        print x
+        printScore xs (n-1)
+
+printLine :: IO ()
+printLine = do
+    putStrLn "-----------------------------------------------"
+
+readScoreList :: FilePath -> IO [Int]
+readScoreList fileName = do
+    score <- readFile fileName
+    return (map read (words score))
